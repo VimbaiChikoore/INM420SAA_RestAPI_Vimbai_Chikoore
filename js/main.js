bf9ca8251cd017516f9664bac4eb7ece3898a82e
VimbaiChikoore <158522823+VimbaiChikoore@users.noreply.github.com>
2024-06-19 21:47:31 -0400

    2

--- js/main.js
+++ js/main.js
@@ -4,10 +4,19 @@ async function getData(apiKey) {
     const data = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`);
     const result = await data.json();
     const image = result.hdurl;
+    const title = result.title;
+    const desc = result.explanation;
 
     
     const imageResult = document.getElementById('apod-image');
     imageResult.innerHTML = `<img src="${image}" alt="nasa image of the day">`
+
+    const titleResult = document.getElementById('apod-title');
+    titleResult.innerHTML = `<p>${title}</p>`
+
+    const descResult = document.getElementById('apod-desc');
+    descResult.innerHTML = `<p>${desc}</p>`
+
     // console.log(image);
 }
 
